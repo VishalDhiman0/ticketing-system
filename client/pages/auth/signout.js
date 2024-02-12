7f211f3598045fce9e1456010b5e4eab54f0a0a3
vishaldhiman <vishaldhiman01999@gmail.com>
2024-02-12 20:23:23 +0530

    add client module

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import useRequest from "../../hooks/use-request";
+import Router from 'next/router';
+
+const Signout = () => {
+    const { doRequest, errors } = useRequest({ url: '/api/users/signout', method: 'post', body: {}, onSuccess: () => Router.push('/') })
+
+    useEffect(() => {
+        doRequest();
+    }, []);
+
+    return <div>Signing out....</div>
+};
+
+export default Signout;
